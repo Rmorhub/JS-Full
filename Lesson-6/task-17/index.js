bc88773d60f60618bee5cb92bac7a7ce57c81538
Rmorhub <rmorphone@gmail.com>
2021-08-07 18:43:42 +0300

    task-17

---
+++ Lesson-6/task-17/index.js
@@ -0,0 +1,17 @@
+// const items = [1,4,1,8,3,4,8,8]
+
+// function uniqueCount(array) {
+//   if (!Array.isArray(array)) {
+//     return null;
+//   }
+//   const unique = [];
+//   for (let index = 0; index < array.length; index += 1) {
+//     const num = array[index];
+//     if (array.includes(num) != unique.includes(num)) {
+//       unique.push(num);
+//     }
+//   }
+//   return unique.length;
+// }
+
+// console.log(uniqueCount(items));
