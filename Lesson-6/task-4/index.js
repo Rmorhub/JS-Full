e9ce72ec1b022fe057b42a89c95232a5a5aaa43b
Rmorhub <rmorphone@gmail.com>
2021-08-07 14:05:22 +0300

    task-4

--- Lesson-6/task-4/index.js
+++ Lesson-6/task-4/index.js
@@ -1,25 +1,25 @@
-/**
- * @param {number[]} numbers
- * @return {number[]}
- */
-function swap(numbers) {
-  const [start, ...rest] = numbers;
-  return [...rest, start];
-}
+// /**
+//  * @param {number[]} numbers
+//  * @return {number[]}
+//  */
+// function swap(numbers) {
+//   const [start, ...rest] = numbers;
+//   return [...rest, start];
+// }
 
-/**
- * @param {number[]} numbers
- * @return {number[]}
- */
-function swapManual(numbers) {
-  let result = [];
-  for (let index = 1; index < numbers.length; index += 1) {
-    result.push(numbers[index]);
-  }
-  result.push(numbers[0]);
-  return result;
-}
+// /**
+//  * @param {number[]} numbers
+//  * @return {number[]}
+//  */
+// function swapManual(numbers) {
+//   let result = [];
+//   for (let index = 1; index < numbers.length; index += 1) {
+//     result.push(numbers[index]);
+//   }
+//   result.push(numbers[0]);
+//   return result;
+// }
 
-// examples
-console.log(swap([1, 10, 9, 11])); // ==> [10, 9, 11, 1]
-console.log(swapManual([1, 10, 9, 11])); // ==> [10, 9, 11, 1]
+// // examples
+// console.log(swap([1, 10, 9, 11])); // ==> [10, 9, 11, 1]
+// console.log(swapManual([1, 10, 9, 11])); // ==> [10, 9, 11, 1]
