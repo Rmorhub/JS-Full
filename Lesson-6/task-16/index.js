56c263cd01e042b2bb95ebeebd9139a5f26f53e1
Rmorhub <rmorphone@gmail.com>
2021-08-07 18:39:44 +0300

    task-16

---
+++ Lesson-6/task-16/index.js
@@ -0,0 +1,18 @@
+// const numbers = [
+//   2, 4, 3, 5, 5, 5, 1, 5, 7, 1, 6, 6, 4, 3, 2, 5, 4, 3, 9, 10, 3, 8, 0,
+// ];
+
+// function removeDuplicates(array) {
+//   if (!Array.isArray(array)) {
+//     return null;
+//   }
+//   const newArray = [];
+//   for (let index = 0; index < array.length; index += 1) {
+//     const num = array[index];
+//     if (array.includes(num) != newArray.includes(num)) {
+//       newArray.push(num);
+//     }
+//   }
+//   return newArray;
+// }
+// console.log(removeDuplicates(numbers));
