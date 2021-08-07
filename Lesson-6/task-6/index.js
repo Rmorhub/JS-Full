8eafc67c6eb5ff6221251c3be96cff90e72baf18
Rmorhub <rmorphone@gmail.com>
2021-08-07 14:33:00 +0300

    task-6

---
+++ Lesson-6/task-6/index.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+function squareArray(arr) {
+  if (!Array.isArray(arr)) {
+    return null;
+  }
+
+  const result = [];
+
+  for (let index = 0; index < arr.length; index += 1) {
+    result.push (arr[index]* arr[index])
+  }
+  return result;
+}
+
+// examples
+console.log(squareArray([1, 10, 9, 11])); // ==> [1, 100, 81, 121]
+console.log(squareArray([10, 0, -4])); // ==> [100, 0, 16]
+console.log(squareArray([1])); // ==> [1]
