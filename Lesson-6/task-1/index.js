f403d65ce2ba2a0e5378d5df4d355062e6a0d3be
Rmorhub <rmorphone@gmail.com>
2021-08-07 13:32:41 +0300

    task-1

---
+++ Lesson-6/task-1/index.js
@@ -0,0 +1,18 @@
+
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+function getArrayBounds(arr) {
+  if (!Array.isArray(arr)) {
+    return null;
+  }
+  const result = [];
+  result.push(arr.length, arr[0], arr[arr.length -1])
+  return result;
+}
+
+// examples
+console.log(getArrayBounds([1, 10, 9, 11])); // ==> [4, 1, 11]
+console.log(getArrayBounds(10, 12, 14)); // ==> null
+console.log(getArrayBounds([1])); // ==> [1, 1, 1]
