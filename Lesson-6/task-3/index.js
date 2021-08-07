d2928bb555aafab0a4d83c45b0790c5e58766ec8
Rmorhub <rmorphone@gmail.com>
2021-08-07 13:46:22 +0300

    task-3

---
+++ Lesson-6/task-3/index.js
@@ -0,0 +1,20 @@
+
+// /**
+//  * @param {number} from
+//  * @param {number} to
+//  * @return {number[]}
+//  */
+// function getSpecialNumbers(from, to) {
+//   let result = [];
+//   for ( let index = from; index <= to; index += 1) {
+//     if ( index % 3 === 0) {
+//       result.push(index)
+//     }
+//   }
+//   return result;
+// }
+
+// // examples
+// console.log(getSpecialNumbers(1, 10)); // ==> [3, 6, 9]
+// console.log(getSpecialNumbers(-10, 10)); // ==> [-9, -6, -3, 0, 3, 6, 9]
+// console.log(getSpecialNumbers(1, 2)); // ==> [ ]
