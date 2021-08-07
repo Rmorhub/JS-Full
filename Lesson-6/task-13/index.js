53a2fb92d8c054c94b0339935f64e04bb9c8ab53
Rmorhub <rmorphone@gmail.com>
2021-08-07 17:52:25 +0300

    task-13

---
+++ Lesson-6/task-13/index.js
@@ -0,0 +1,19 @@
+// const clients = ['Ann', 'John', 'User'];
+// const balances = [1400, 87, -6];
+
+// function withdraw(clients, balances, client, amount) {
+//   for (let index = 0; index <= clients.length; index += 1) {
+//     if (clients[index] === client) {
+//       if (balances[index] < amount) {
+//         return -1;
+//       } else {
+//         let balanceAfter = balances[index] - amount;
+//         balances[index] = balanceAfter;
+//       return balanceAfter;
+//       }
+//     }
+//   }
+// }
+
+// console.log(withdraw(clients, balances, 'John', 50));
+// console.log(withdraw(clients, balances, 'User', 10));
