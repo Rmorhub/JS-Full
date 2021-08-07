e3a238f683c7448b170e3210f533c93abee1eff8
Rmorhub <rmorphone@gmail.com>
2021-08-07 12:09:27 +0300

    task-8

---
+++ Lesson-5/task-8/index.js
@@ -0,0 +1,35 @@
+// function compareSums(a, b, c, d) {
+//   const firstSum = sum(a, b);
+//   const secondSum = sum(c, d);
+//   return sum(a, b) > sum(c, d);
+// }
+
+// function sum(from, to) {
+//   let sum = 0;
+//   for (let index = from; index <= to; index += 1) {
+//     sum += index;
+//   }
+//   return sum;
+// }
+
+// console.log(sum(1, 4));
+
+// console.log(compareSums(1,6,1,5))
+
+
+
+// function compareSums(a, b, c, d) {
+//   return sum(a, b) > sum(c, d);
+// }
+
+// function sum(from, to) {
+//   let sum = 0;
+//   for (let index = from; index <= to; index += 1) {
+//     sum += index;
+//   }
+//   return sum;
+// }
+
+// console.log(sum(1, 4));
+
+// console.log(compareSums(1, 3, 1, 5));
