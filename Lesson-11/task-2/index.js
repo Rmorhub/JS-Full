f7ef32c8436454e2135352e357407feb82597110
Rmorhub <rmorphone@gmail.com>
2021-08-18 20:19:26 +0300

    task-2

---
+++ Lesson-11/task-2/index.js
@@ -0,0 +1,45 @@
+const contacts = [
+  {
+    name: 'Tom',
+    phoneNumber: '777-77-77',
+  },
+  {
+    name: 'Helen',
+    phoneNumber: '333-33-33',
+  },
+  {
+    name: 'John',
+    phoneNumber: '111-11-11',
+  },
+  {
+    name: 'Kate',
+    phoneNumber: '222-22-22',
+  },
+  {
+    name: 'Bob',
+    phoneNumber: '999-99-99',
+  },
+  {
+    name: 'Alex',
+    phoneNumber: '444-44-44',
+  },
+];
+
+// const sortContacts = (arr, boolean = true) => {
+//   if (!Array.isArray(arr)) {
+//     return null;
+//   }
+
+//   const result = arr.sort((a, b) => {
+//     if (boolean === true) {
+//       return a.name.localeCompare(b.name);
+//     } else {
+//       return b.name.localeCompare(a.name);
+//     }
+//   });
+//   return result;
+// };
+
+// console.log(sortContacts(contacts));
+// console.log(sortContacts(contacts, true));
+// console.log(sortContacts(contacts, false));
