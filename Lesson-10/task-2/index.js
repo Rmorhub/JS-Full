79a8aac783d17a00995b81ee0398b771b66bc07d
Rmorhub <rmorphone@gmail.com>
2021-08-18 14:08:44 +0300

    task-2

---
+++ Lesson-10/task-2/index.js
@@ -0,0 +1,16 @@
+// const arrList = [1, 2, 3, '4', 'five', 'six', NaN, undefined, -Infinity, null,11.4,19.9,0.1,'17l','19,4','19.78'];
+
+// const getParsedIntegers = (arr) => arr.map((el) => Number.parseInt(el));
+// console.log(getParsedIntegers(arrList));
+
+// const getParsedIntegersV2 = (arr) => arr.map((el) => parseInt(el));
+// console.log(getParsedIntegersV2(arrList));
+
+// const getParsedFloats = (arr) => arr.map((el) => Number.parseFloat(el));
+// console.log(getParsedFloats(arrList));
+
+// const getParsedFloatsV2 = (arr) => arr.map((el) => parseFloat(el));
+// console.log(getParsedFloatsV2(arrList));
+
+
+// console.log(arrList)
