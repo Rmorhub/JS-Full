3ea6f75937a693485b22905a03fad12777a32f0f
Rmorhub <rmorphone@gmail.com>
2021-08-19 22:08:32 +0300

    task-5

--- Lesson-13/task-5/task-5.js
+++ Lesson-13/task-5/task-5.js
@@ -1,47 +1,47 @@
-// reverseArray
-const numbers = [1, 5, 6, 9, 10, 12, 15];
+// // reverseArray
+// const numbers = [1, 5, 6, 9, 10, 12, 15];
 
-export const reverseArray = (arr) =>
-  Array.isArray(arr) ? arr.slice().reverse() : null;
+// export const reverseArray = (arr) =>
+//   Array.isArray(arr) ? arr.slice().reverse() : null;
 
-console.log(reverseArray(numbers));
-console.log(reverseArray());
-console.log(reverseArray('Test'));
-console.log(numbers);
+// console.log(reverseArray(numbers));
+// console.log(reverseArray());
+// console.log(reverseArray('Test'));
+// console.log(numbers);
 
-// withdraw
+// // withdraw
 
-const clients = ['Ann', 'John', 'User'];
-const balances = [1400, 87, -6];
+// const clients = ['Ann', 'John', 'User'];
+// const balances = [1400, 87, -6];
 
-export const withdraw = (clients, balances, client, amount) => {
-  let index = clients.indexOf(client);
-  if (balances[index] < amount) {
-    return -1;
-  } else {
-    balances[index] = balances[index] - amount;
-  }
-  return balances[index];
-};
+// export const withdraw = (clients, balances, client, amount) => {
+//   let index = clients.indexOf(client);
+//   if (balances[index] < amount) {
+//     return -1;
+//   } else {
+//     balances[index] = balances[index] - amount;
+//   }
+//   return balances[index];
+// };
 
-console.log(withdraw(clients, balances, 'John', 50));
-console.log(withdraw(clients, balances, 'User', 10));
-console.log(withdraw(clients, balances, 'John', )); 
+// console.log(withdraw(clients, balances, 'John', 50));
+// console.log(withdraw(clients, balances, 'User', 10));
+// console.log(withdraw(clients, balances, 'John', )); 
 
-// getAdults
+// // getAdults
 
-export const getAdults = (obj) => {
-  let result = {};
+// export const getAdults = (obj) => {
+//   let result = {};
 
-  for (let key in obj) {
-    if (obj[key] >= 18) {
-      result[key] = obj[key];
-    }
-  }
-  return result;
-};
+//   for (let key in obj) {
+//     if (obj[key] >= 18) {
+//       result[key] = obj[key];
+//     }
+//   }
+//   return result;
+// };
 
 
-console.log(getAdults({ 'John Doe': 19, Tom: 17, Bob: 18 }));
-console.log(getAdults({ Ann: 56, Andrey: 7 }));
-console.log(getAdults());
+// console.log(getAdults({ 'John Doe': 19, Tom: 17, Bob: 18 }));
+// console.log(getAdults({ Ann: 56, Andrey: 7 }));
+// console.log(getAdults());
