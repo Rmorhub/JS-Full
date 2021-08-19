12718a62f21c00fdc463aacbc88f39b80cfdc786
Rmorhub <rmorphone@gmail.com>
2021-08-19 17:04:03 +0300

    task-4

--- Lesson-11/task-4/index.js
+++ Lesson-11/task-4/index.js
@@ -1,28 +1,28 @@
-const splitString = (text, len = 10) => {
-  if (!(typeof text === 'string')) {
-    return null;
-  }
-  const strArr = [];
-  let startPosition = 0;
-  let sameText = text.slice();
+// const splitString = (text, len = 10) => {
+//   if (!(typeof text === 'string')) {
+//     return null;
+//   }
+//   const strArr = [];
+//   let startPosition = 0;
+//   let sameText = text.slice();
 
-  while (sameText.length % len) {
-    sameText += '.';
-  }
+//   while (sameText.length % len) {
+//     sameText += '.';
+//   }
 
-  while (true) {
-    let chunk = sameText.substr(startPosition, len);
-    if (chunk.length === 0) {
-      break;
-    }
-    strArr.push(chunk);
-    startPosition += len;
-  }
+//   while (true) {
+//     let chunk = sameText.substr(startPosition, len);
+//     if (chunk.length === 0) {
+//       break;
+//     }
+//     strArr.push(chunk);
+//     startPosition += len;
+//   }
 
-  return strArr;
-};
+//   return strArr;
+// };
 
-console.log(splitString('asdasdad asdasdas'));
-console.log(splitString(1, 3));
-console.log(splitString('asdad aggdsfsdfsfsdfsdf sdfsdfsdf'));
-console.log(splitString('asdada ggdsfsdfsfsdf sdfsdfsdfsdf', 4));
+// console.log(splitString('asdasdad asdasdas'));
+// console.log(splitString(1, 3));
+// console.log(splitString('asdad aggdsfsdfsfsdfsdf sdfsdfsdf'));
+// console.log(splitString('asdada ggdsfsdfsfsdf sdfsdfsdfsdf', 4));
