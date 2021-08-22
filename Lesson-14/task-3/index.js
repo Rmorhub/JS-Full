b3caafbb3efb8f8b11fcf5f80c6e15c47971d9e1
Rmorhub <rmorphone@gmail.com>
2021-08-22 11:51:59 +0300

    task-3

---
+++ Lesson-14/task-3/index.js
@@ -0,0 +1,21 @@
+// let memory = 0;
+
+// export const add = num => (memory += num);
+
+// add(10);
+// console.log(memory);
+
+// export const decrease = num => (memory -= num);
+
+// decrease(2);
+// console.log(memory);
+
+// export const reset = () => (memory = 0);
+
+// reset();
+// console.log(memory);
+
+// export const getMemo = () => memory;
+
+// getMemo();
+// console.log(memory);
