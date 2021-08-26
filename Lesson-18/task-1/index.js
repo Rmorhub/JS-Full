edfb5196b89f7748fa7cac987273cc0429788879
Rmorhub <rmorphone@gmail.com>
2021-08-26 09:31:43 +0300

    task-1/context(this)-part2

---
+++ Lesson-18/task-1/index.js
@@ -0,0 +1,14 @@
+export const event = {
+  guests: [{ name: `John`, age: `18`, email: `example@server.com` }],
+  message: `Welcome to the party!`,
+  getInvitations() {
+    return this.guests
+      .filter(({ age }) => age >= 18)
+      .map(({ name, email }) => ({
+        email,
+        message: `Dear ${name}! ${this.message}`,
+      }));
+  },
+};
+
+console.log(event.getInvitations());
