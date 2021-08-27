2072c0504430113b6669f566e1a890f0eca1abcd
Rmorhub <rmorphone@gmail.com>
2021-08-27 16:02:17 +0300

    task-3

--- Lesson-18/task-3/index.js
+++ Lesson-18/task-3/index.js
@@ -1,5 +1,4 @@
-function sumOfSquares() {
-  // eslint-disable-next-line prefer-rest-params
-  return [...arguments].map(el => el * el).reduce((acc, elem) => acc + elem);
+function sumOfSquares(...arg) {
+  return [...arg].map(el => el * el).reduce((acc, elem) => acc + elem);
 }
 console.log(sumOfSquares(4, 4, 4, 5, 10));
