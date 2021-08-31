a0400e394dc5084dcf7d89b518115c9b4b9125a0
Rmorhub <rmorphone@gmail.com>
2021-08-31 15:46:54 +0300

    task-2

---
+++ Lesson-21/task-2/index.js
@@ -0,0 +1,28 @@
+// to do
+// создать ф-цию 1
+// которая ищет на странице эл с классом .title
+// вывести найденный эл в консоль console.dir(titleElem)
+// return el
+
+// создать ф-цию 2
+// которая найдет на странице input с типом text
+// выведет найденный эл в консоль с помощью console.dir(inputElem)
+// return el
+
+// поиск с помощью метода document.querySelector()
+
+const getTitleElement = () => {
+  const titleElem = document.querySelector('.title');
+  console.dir(titleElem);
+  return titleElem;
+};
+
+console.log(getTitleElement());
+
+const getInputElement = () => {
+  const inputElem = document.querySelector('input');
+  console.dir(inputElem);
+  return inputElem;
+};
+
+console.log(getInputElement());
