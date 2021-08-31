321b5fbf472f1a38547606d4aad196069c8af3a3
Rmorhub <rmorphone@gmail.com>
2021-08-31 17:42:22 +0300

    task-5

---
+++ Lesson-21/task-5/index.js
@@ -0,0 +1,16 @@
+// to do
+// 1 создать ф-цию setTitle принимает 1аргумент (text) 'стринг'
+// которая ищет на странице эл с классом .title
+// и устанавливает в него заданный текст (text)
+
+// поиск с помощью метода document.querySelector()
+// использовать textContent;                               <--------------------
+// newProductElem.textContent = 'New Product';             <--------------------
+// не забыть экспортировать под своими именами
+
+export const setTitle = (text) => {
+  const result = document.querySelector('.title');
+  result.textContent = text;
+};
+
+setTitle('new-title');
