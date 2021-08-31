45e3a033d8745198dbd6ad23887417ae4157553c
Rmorhub <rmorphone@gmail.com>
2021-08-31 14:37:00 +0300

    task-1

--- Lesson-21/task-1/index.js
+++ Lesson-21/task-1/index.js
@@ -1,10 +1,15 @@
-const tasks = [
+export const tasks = [
   { text: 'Buy milk', done: false },
   { text: 'Pick up Tom from airport', done: false },
   { text: 'Visit party', done: false },
   { text: 'Visit doctor', done: true },
   { text: 'Buy meat', done: true },
 ];
+
+/**
+ * @param {object[]} tasksList
+ * @return {undefined}
+ */
 
 const renderListItems = listItems => {
   const listElem = document.querySelector('.list');
