7b1e0f40c51df004a4dfa462ebc24b4d3f367b56
Rmorhub <rmorphone@gmail.com>
2021-09-01 21:20:28 +0300

    task-10

---
+++ Lesson-22/task-10/index.js
@@ -0,0 +1,10 @@
+const buttons = document.querySelectorAll('.pagination__page');
+const button = Array.from(buttons);
+
+const handleClick = event => {
+  console.log(event.target.getAttribute('data-page-number'));
+};
+
+button[0].addEventListener('click', handleClick);
+button[1].addEventListener('click', handleClick);
+button[2].addEventListener('click', handleClick);
