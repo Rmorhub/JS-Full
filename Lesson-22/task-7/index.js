8e6bddb731857585c972b625ccc87b11d68facba
Rmorhub <rmorphone@gmail.com>
2021-09-01 20:08:12 +0300

    task-7

--- Lesson-22/task-7/index.js
+++ Lesson-22/task-7/index.js
@@ -2,7 +2,8 @@ const buttons = document.querySelectorAll('.btn');
 const button = Array.from(buttons);
 
 const handleClick = () => {
-  console.log(event.target.textContent);
+  const buttonName = event.target.textContent;
+  console.log(buttonName);
 };
 
 button[0].addEventListener('click', handleClick);
