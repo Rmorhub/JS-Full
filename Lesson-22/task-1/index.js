38762657d5d615e5d5b49281d64668d8c4aa671e
Rmorhub <rmorphone@gmail.com>
2021-09-01 21:53:24 +0300

    task-1

--- Lesson-22/task-1/index.js
+++ Lesson-22/task-1/index.js
@@ -1,12 +1,10 @@
+// ================================================================
 const divElem = document.querySelector('.rect_div');
-console.log(divElem);
 const pElem = document.querySelector('.rect_p');
 const spanElem = document.querySelector('.rect_span');
-
-
+const eventListElem = document.querySelector('.events-list');
+// ================================================================
 const logTarget = (text, color) => {
-  const eventListElem = document.querySelector('.events-list');
-
   eventListElem.innerHTML += `<span style="color: ${color}; margin-left: 8px">${text}</span>`;
 };
 
@@ -18,11 +16,46 @@ const logGreyDiv = logTarget.bind(null, 'DIV', 'grey');
 const logGreyP = logTarget.bind(null, 'P', 'grey');
 const logGreySpan = logTarget.bind(null, 'SPAN', 'grey');
 
-divElem.addEventListener('click', logGreyDiv, true);
-divElem.addEventListener('click', logGreenDiv);
+// =========================clearBtn================================
 
-pElem.addEventListener('click', logGreyP, true);
-pElem.addEventListener('click', logGreenP);
+const clearBtn = document.querySelector('.clear-btn');
 
-spanElem.addEventListener('click', logGreySpan, true);
-spanElem.addEventListener('click', logGreenSpan);
+const cleanWindow = () => {
+  eventListElem.textContent = '';
+};
+
+clearBtn.addEventListener('click', cleanWindow);
+
+// ===========================attachBtn===================================
+
+const attachBtn = document.querySelector('.attach-handlers-btn');
+
+const attachSquare = () => {
+  divElem.addEventListener('click', logGreyDiv, true);
+  divElem.addEventListener('click', logGreenDiv);
+
+  pElem.addEventListener('click', logGreyP, true);
+  pElem.addEventListener('click', logGreenP);
+
+  spanElem.addEventListener('click', logGreySpan, true);
+  spanElem.addEventListener('click', logGreenSpan);
+};
+
+attachBtn.addEventListener('click', attachSquare);
+
+// =========================removeBtn===============================
+
+const removeBtn = document.querySelector('.remove-handlers-btn');
+
+const removeSquare = () => {
+  divElem.removeEventListener('click', logGreyDiv, true);
+  divElem.removeEventListener('click', logGreenDiv);
+
+  pElem.removeEventListener('click', logGreyP, true);
+  pElem.removeEventListener('click', logGreenP);
+
+  spanElem.removeEventListener('click', logGreySpan, true);
+  spanElem.removeEventListener('click', logGreenSpan);
+};
+
+removeBtn.addEventListener('click', removeSquare);
