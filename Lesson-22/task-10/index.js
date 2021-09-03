b0b781a9bc18ee60427e7764f849f9bbf2cf537f
Rmorhub <rmorphone@gmail.com>
2021-09-03 20:03:11 +0300

    task-10

--- Lesson-22/task-10/index.js
+++ Lesson-22/task-10/index.js
@@ -5,6 +5,8 @@ const handleClick = event => {
   console.log(event.target.getAttribute('data-page-number'));
 };
 
-button[0].addEventListener('click', handleClick);
-button[1].addEventListener('click', handleClick);
-button[2].addEventListener('click', handleClick);
+// button[0].addEventListener('click', handleClick);
+// button[1].addEventListener('click', handleClick);
+// button[2].addEventListener('click', handleClick);
+
+button.forEach(elem => elem.addEventListener('click', handleClick));
