56e240f3bced06557f90d8ace4c1fbfb328b4712
Rmorhub <rmorphone@gmail.com>
2021-09-07 14:51:14 +0300

    task-1

---
+++ Lesson-24/task-1/index.js
@@ -0,0 +1,14 @@
+const weekDays = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'St'];
+
+const dayOfWeek = (date, days) => {
+  const day = new Date(date).getDate();
+  const dateInFuture = new Date(date).setDate(day + days);
+
+  return weekDays[new Date(dateInFuture).getDay()];
+};
+
+const someDate = new Date(2021, 8, 7);
+
+const result = dayOfWeek(someDate, 15);
+console.log(someDate);
+console.log(result);
