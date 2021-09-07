9ee25d6cf6b6ec7912eef16924e28ca6a00f1184
Rmorhub <rmorphone@gmail.com>
2021-09-07 17:32:11 +0300

    task-3

--- Lesson-24/task-3/index.js
+++ Lesson-24/task-3/index.js
@@ -1,19 +1,14 @@
 export const getDiff = (startDate, endDate) => {
-  let days = null;
-  let hours = null;
-  let minutes = null;
-  let seconds = null;
+  let diff = null;
   if (startDate > endDate) {
-    days = (startDate - endDate) / 86400000;
-    hours = new Date(startDate - endDate).getUTCHours();
-    minutes = new Date(startDate - endDate).getMinutes();
-    seconds = new Date(startDate - endDate).getSeconds();
+    diff = startDate - endDate;
   } else {
-    days = (endDate - startDate) / 86400000;
-    hours = new Date(endDate - startDate).getUTCHours();
-    minutes = new Date(endDate - startDate).getMinutes();
-    seconds = new Date(endDate - startDate).getSeconds();
+    diff = endDate - startDate;
   }
+  const days = diff / 86400000;
+  const hours = new Date(diff).getUTCHours();
+  const minutes = new Date(diff).getMinutes();
+  const seconds = new Date(diff).getSeconds();
   const res = `${Math.floor(days)}d ${hours}h ${minutes}m ${seconds}s`;
   return res;
 };
@@ -28,3 +23,23 @@ const fourthDate = new Date(2020, 1, 3, 0, 0, 0);
 
 console.log(getDiff(firstDate, secondDate));
 console.log(getDiff(thirdtDate, fourthDate));
+
+// const getDiff = (startDate, endDate) => {
+//   let days = null;
+//   let hours = null;
+//   let minutes = null;
+//   let seconds = null;
+//   if (startDate > endDate) {
+//     days = (startDate - endDate) / 86400000;
+//     hours = new Date(startDate - endDate).getUTCHours();
+//     minutes = new Date(startDate - endDate).getMinutes();
+//     seconds = new Date(startDate - endDate).getSeconds();
+//   } else {
+//     days = (endDate - startDate) / 86400000;
+//     hours = new Date(endDate - startDate).getUTCHours();
+//     minutes = new Date(endDate - startDate).getMinutes();
+//     seconds = new Date(endDate - startDate).getSeconds();
+//   }
+//   const res = `${Math.floor(days)}d ${hours}h ${minutes}m ${seconds}s`;
+//   return res;
+// };
