2df4b34aeca603b6932428b6442982b702d78a24
Rmorhub <rmorphone@gmail.com>
2021-09-08 15:27:03 +0300

    task-1

--- Lesson-27/task-1/index.js
+++ Lesson-27/task-1/index.js
@@ -4,7 +4,7 @@ localStorage.setItem('hobbies', JSON.stringify({ name: 'Tom' }));
 localStorage.setItem('age', JSON.stringify(17));
 
 // eslint-disable-next-line arrow-body-style
-const getLocalStorageData = () => {
+export const getLocalStorageData = () => {
   // eslint-disable-next-line arrow-body-style
   return Object.entries(localStorage).reduce((acc, [key, value]) => {
     return {
