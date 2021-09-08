6e7b9fa7dcf706abb4b4ca87d65f7efd0a4ab2b2
Rmorhub <rmorphone@gmail.com>
2021-09-08 15:40:16 +0300

    task-1

--- Lesson-27/task-1/index.js
+++ Lesson-27/task-1/index.js
@@ -1,24 +1,23 @@
-// localStorage.clear();
-// localStorage.setItem('hobbies', JSON.stringify({ name: 'Tom' }));
-// localStorage.setItem('name', 'Tom');
-// localStorage.setItem('age', JSON.stringify(17));
+localStorage.clear();
+localStorage.setItem('hobbies', JSON.stringify({ name: 'Tom' }));
+localStorage.setItem('name', 'Tom');
+localStorage.setItem('age', JSON.stringify(17));
 
 // eslint-disable-next-line arrow-body-style
 export const getLocalStorageData = () => {
   // eslint-disable-next-line arrow-body-style
   return Object.entries(localStorage).reduce((acc, [key, value]) => {
+    let newValue;
+    try {
+      newValue = JSON.parse(value);
+    } catch (e) {
+      newValue = value;
+    }
     return {
       ...acc,
-      [key]: value,
+      [key]: newValue,
     };
   }, {});
 };
 
-// console.log(getLocalStorageData());
-
-// let newValue;
-// try {
-//   newValue = JSON.stringify(value);
-// } catch (e) {
-//   newValue = value;
-// }
+console.log(getLocalStorageData());
