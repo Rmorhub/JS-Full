2e1cabe143dd3da68495a81544624d44e96db9a7
Rmorhub <rmorphone@gmail.com>
2021-09-16 16:24:27 +0300

    task-3

---
+++ Lesson-36/task-3/index.js
@@ -0,0 +1,14 @@
+const fetchUserData = userName =>
+  fetch(`https://api.github.com/users/${userName}`).then(response => response.json());
+
+export const getUsersBlogs = async userName => {
+  const usersArray = userName;
+  try {
+    return await Promise.all(usersArray.map(user => fetchUserData(user).then(users => users.blog)));
+  } catch (error) {
+    throw new Error('Failed to fetch user');
+  }
+};
+
+getUsersBlogs(['google', 'facebook', 'gaearon']).then(linksList => console.log(linksList));
+// ["https://opensource.google/", "https://opensource.fb.com", "http://twitter.com/dan_abramov"]
