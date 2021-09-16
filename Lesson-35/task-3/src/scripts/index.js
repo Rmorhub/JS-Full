b351b651e5f8060475941f0d08b1d64bc62dcb5b
Rmorhub <rmorphone@gmail.com>
2021-09-16 12:58:21 +0300

    task-3

--- Lesson-35/task-3/src/scripts/index.js
+++ Lesson-35/task-3/src/scripts/index.js
@@ -1,10 +1,10 @@
 import { fetchUserData, fetchRepositories } from './gateways.js';
 import { renderUserData } from './user.js';
-import { cleanReposList, renderRepos } from './repos.js';
+import { cleanReposList, renderRepos } from './reposi.js';
 import { showSpinner, hideSpinner } from './spinner.js';
 
 const defaultUser = {
-  avatar_url: 'https://avatars3.githubusercontent.com/a',
+  avatar_url: 'https://avatars3.githubusercontent.com/u10001',
   name: '',
   location: '',
 };
