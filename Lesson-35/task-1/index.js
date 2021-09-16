523fe6c33b8488a2f9dce60ccc31fffbbcc738b9
Rmorhub <rmorphone@gmail.com>
2021-09-16 10:33:51 +0300

    task-1

---
+++ Lesson-35/task-1/index.js
@@ -0,0 +1,27 @@
+window.addEventListener('error', err => {
+  console.error('error', err.message);
+});
+
+let userParsingResult;
+
+try {
+  const user = JSON.parse('{"name":"Tom","city":"New-York"}');
+  console.log('User data: ', user);
+  userParsingResult = 'success';
+} catch (e) {
+  userParsingResult = 'error';
+} finally {
+  console.log(`User parsing finished with ${userParsingResult}`);
+}
+
+let productParsingResult;
+
+try {
+  const product = JSON.parse('{"name":"Tv-50","brand":"Samsung"');
+  console.log('Product data: ', product);
+  productParsingResult = 'success';
+} catch (e) {
+  productParsingResult = 'error';
+} finally {
+  console.log(`Product parsing finished with ${productParsingResult}`);
+}
