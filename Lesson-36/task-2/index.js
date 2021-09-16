324f683c4db88445858794ef853d9f6b24989fec
Rmorhub <rmorphone@gmail.com>
2021-09-16 15:26:59 +0300

    task-2

---
+++ Lesson-36/task-2/index.js
@@ -0,0 +1,36 @@
+import { fetchUserData, fetchRepositories } from './gateways.js';
+import { renderUserData } from './user.js';
+import { cleanReposList, renderRepos } from './reposi.js';
+import { showSpinner, hideSpinner } from './spinner.js';
+
+const defaultUser = {
+  avatar_url: 'https://avatars3.githubusercontent.com/u10001',
+  name: '',
+  location: '',
+};
+
+renderUserData(defaultUser);
+
+const showUserBtnElem = document.querySelector('.name-form__btn');
+const userNameInputElem = document.querySelector('.name-form__input');
+
+const onSearchUser = async () => {
+  showSpinner();
+  cleanReposList();
+  const userName = userNameInputElem.value;
+  try {
+    const userData = await fetchUserData(userName);
+    renderUserData(userData);
+    const reposList = await fetchRepositories(userData.repos_url);
+    renderRepos(reposList);
+  } catch (error) {
+    alert(error.message);
+  } finally {
+    hideSpinner();
+  }
+};
+
+showUserBtnElem.addEventListener('click', onSearchUser);
+
+// 'https://api.github.com/users/USERNAME'
+// fetch and render userdata ...
